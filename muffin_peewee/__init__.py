94de41e5c53f1b28a05408275e4aad956ab9d3b9
Kirill Klenov <horneds@gmail.com>
2016-04-04 11:17:23 -0700

    Bump version: 1.0.3 → 1.0.4

--- muffin_peewee/__init__.py
+++ muffin_peewee/__init__.py
@@ -3,7 +3,7 @@
 # Package information
 # ===================
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __project__ = "muffin-peewee"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
