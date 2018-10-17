49901042b7c07a367f86aec8112d0737105a1078
Kirill Klenov <horneds@gmail.com>
2018-10-17 22:03:43 +0300

    fix tests.

--- tests.py
+++ tests.py
@@ -135,7 +135,6 @@ async def test_async_peewee(model):
 
     with (await app.ps.peewee.manage()):
         assert app.ps.peewee.database.obj.execution_context_depth() == 1
-        model.create_table()
         model.select().execute()
 
     assert app.ps.peewee.database.obj.execution_context_depth() == 0
