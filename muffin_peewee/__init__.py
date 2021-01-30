e9e05515541a5cd0c44eeefbd556a6157c086198
Kirill Klenov <horneds@gmail.com>
2021-01-31 00:00:02 +0300

    Bump version: 1.5.2 → 1.6.0

--- muffin_peewee/__init__.py
+++ muffin_peewee/__init__.py
@@ -11,7 +11,7 @@ from muffin.plugin import BasePlugin
 from peewee_migrate import Router
 
 
-__version__ = "1.5.2"
+__version__ = "1.6.0"
 __project__ = "muffin-peewee"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
