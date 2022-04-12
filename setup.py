c57aea3ec115eeafb106db734d6c81185e6f788b
Kirill Klenov <horneds@gmail.com>
2022-04-12 09:20:26 +0300

    fix(build): fix actions

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
-#!/usr/bin/env python
+"""Setup the package."""
+
+# Parse requirements
+# ------------------
+import pathlib
+
+import pkg_resources
+
+
+def parse_requirements(path: str) -> "list[str]":
+    with pathlib.Path(path).open() as requirements:
+        return [str(req) for req in pkg_resources.parse_requirements(requirements)]
+
+
+# Setup package
+# -------------
 
 from setuptools import setup
 
-setup()
+setup(
+    install_requires=parse_requirements("requirements.txt"),
+)
